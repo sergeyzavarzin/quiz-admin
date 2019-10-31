419156cacf2d6181e4d21115c6cfbdea5242c01e
Sergey <tweek4000@yandex.ru>
2019-11-01 01:12:23 +0400

    Fix: Fix base api url

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = 'http://192.168.0.200:8080';
+export const BASE_API_URL = process.env.NODE_ENV === 'development' ?
+  'http://192.168.0.200:8080' : 'https://zen-quiz-api.herokuapp.com';
