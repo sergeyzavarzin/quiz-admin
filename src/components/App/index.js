f3cc8aedabdeef5194e2815c6f3b05cad9cce9e4
Sergey <tweek4000@yandex.ru>
2019-11-24 22:21:13 +0400

    Feat: Logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import {withRouter, Route, Link} from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import {Layout, Menu} from 'antd';
 import 'antd/dist/antd.css';
 
 import Matches from '../../pages/Matches';
 import Rivals from '../../pages/Rivals';
 import Merch from '../../pages/Merch';
 
-const { Header, Content, Footer } = Layout;
-const { Item } = Menu;
+import Logo from '../../img/logo.svg';
+
+const {Header, Content, Footer} = Layout;
+const {Item} = Menu;
 
 const App = ({location: {pathname}}) => {
   return (
     <Layout style={{minHeight: '100vh'}}>
-      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-        <div className='logo' />
+      <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+        <img className='logo' src={Logo} alt='Зенит' style={{width: 80}}/>
         <Menu
           theme='dark'
           mode='horizontal'
           defaultSelectedKeys={[pathname.slice(1)]}
           defaultOpenKeys={[pathname.slice(1)]}
-          style={{ lineHeight: '64px' }}
+          style={{lineHeight: '64px', float: 'right'}}
         >
           <Item key='matches'>
             <Link to='/matches'>Матчи</Link>
@@ -33,14 +35,14 @@ const App = ({location: {pathname}}) => {
           </Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '0 50px', marginTop: 64 }}>
-        <div style={{ padding: 24, marginTop: 30, background: '#fff', minHeight: 360 }}>
+      <Content style={{padding: '0 50px', marginTop: 64}}>
+        <div style={{padding: 24, marginTop: 30, background: '#fff', minHeight: 360}}>
           <Route path={['/', '/matches']} exact component={Matches}/>
           <Route path='/rivals' exact component={Rivals}/>
           <Route path='/merch' exact component={Merch}/>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>tweek4000@yandex.ru ©2019</Footer>
+      <Footer style={{textAlign: 'center'}}>tweek4000@yandex.ru ©2019</Footer>
     </Layout>
   );
 };
